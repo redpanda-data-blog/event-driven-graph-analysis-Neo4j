cc2d27e28e5a7d47aed8819804d0910dae31b5dd
Rajkumar Venkatasamy <rajkumar.venkatasamy@navis.com>
2023-05-10 22:16:51 +0530

    Updated comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,7 +18,7 @@ public class App {
     private static final String TOPIC = "movies-directed-by";
     private static final ObjectMapper objectMapper = new ObjectMapper();
     public static void main(String[] args)  {
-        // Set the properties for the Consumer
+        // Set the properties for the consumer
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("group.id", "test");
@@ -33,10 +33,10 @@ public class App {
         // Establish a connection to the Neo4j database
         try (Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "password"))) {
             while (true) {
-                // Consumer that polls for records from the subscribed topic
+                // Add a consumer that polls for records from the subscribed topic
                 // The consumed messages are processed by extracting the values for the fields "movieId", "movieName",
                 // "directorName", and "year". These fields are then passed to the createMovieDirectedBy method to
-                // make entries in the Neo4j graph database
+                // make entries in the Neo4j graph database.
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records) {
                     JsonNode jsonNode = objectMapper.readTree(record.value());
@@ -52,8 +52,8 @@ public class App {
         }
     }
 
-    // createMovieDirectedBy method creates a new node for the Movie and also creates a new node for the Director.
-    // It then creates a relationship between the Director node and the Movie node with the label DIRECTED and a year
+    // The createMovieDirectedBy method creates a new node for the movie and also creates a new node for the director.
+    // It then creates a relationship between the director node and the movie node with the label DIRECTED and a year
     // property with the provided year value.
     private static void createMovieDirectedBy(Driver driver, String movieId, String movieName, String directorName, int year) {
         try (Session session = driver.session()) {
